cb78628259bb42c06c5491324e7abd49eb97db04
Gabrielle Malate <146153431+gabriellemalate@users.noreply.github.com>
2024-08-20 00:17:02 -0700

    ts to js. necessary colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,8 +1,7 @@
-/**
+/*
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
+ 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
@@ -23,4 +22,11 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+};
+*/
+
+export default {
+    primary: "#0a4d4a",
+    grey: "#5E5D5E",
+    dark: "#1A1A1A",
 };
